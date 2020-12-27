d94a99bec5dbdb7f447b10189c3c441eef91e2d5
Negin Naserzadeh <72472009+nigkty123@users.noreply.github.com>
2020-12-27 21:53:26 +0330

    found some ansi code online to make it pretty

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,7 +9,41 @@
 #include <unordered_set>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>
+#include <thread>
+#include <chrono>
+#include <sstream>
 
+enum class ansi_color_code: int{
+    black = 30,
+    red=31,
+    green=32,
+    yellow=33,
+    blue=34,
+    magenta=35,
+    cyan=36,
+    white=37,
+    bright_black = 90,
+    bright_red=91,
+    bright_green=92,
+    bright_yellow=93,
+    bright_blue=94,
+    bright_magenta=95,
+    bright_cyan=96,
+    bright_white=97,
+};
+
+template<typename printable>
+std::string print_as_color(printable const& value, ansi_color_code color){
+    std::stringstream sstr;
+    sstr<<"\033[1;"<<static_cast<int>(color)<<"m"<<value<<"\033[0m";
+    return sstr.str();
+}
+template<ansi_color_code color,typename printable>
+std::string print_as_color(printable const& value){
+    std::stringstream sstr;
+    sstr<<"\033[1;"<<static_cast<int>(color)<<"m"<<value<<"\033[0m";
+    return sstr.str();
+}
 
 int findFreeX(int puzzle[WIDTH][HEIGHT])
 {
@@ -252,8 +286,8 @@ int main()
     BFS(goal,Qagenda,Qclosed,solution);
 
     std::cout<<"BFS Solution: "<<std::endl;
-    std::cout<<solution.getPath()<<std::endl;
-    std::cout<<"number of moves:"<<solution.no_OfMoves()<<std::endl;
+    std::cout<<print_as_color<ansi_color_code::blue>(solution.getPath())<<std::endl;
+    std::cout<<"number of moves:"<<print_as_color<ansi_color_code::blue>(solution.no_OfMoves())<<std::endl;
     
     
     DFS(goal,agenda,closed,solution);
